9bd0be748071e15ea65f5d8ec5c30961c0d19074
Александр <86889942+AlxC0de@users.noreply.github.com>
2025-10-19 14:37:58 +0500

    Add divide function

--- calculator.py
+++ calculator.py
@@ -3,5 +3,10 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
+    
+def divide(a, b):
+    if b != 0:
+        return a / b
+    return "Error: Division by zero"
 
 print("Calculator v1.0")
